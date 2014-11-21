0315ec3b67509b733e6069bf4fcaeaaed9a757aa
Alexander Sadovsky <alino.sadovsky@gmail.com>
2014-11-21 01:03:49 +0100

    Authentication in progress

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
-taskybird-webserver
-===================
+# activityOverload
 
-This is a task flow server which sends websockets to thunderbird clients.
-Currently in development - there is no working release yet.
-
-##Installation:
-`git clone git@github.com:Alino/taskybird-webserver.git`
-
-`cd taskybird-webserver`
-
-`npm install`
-
-##Thunderbird extension
-If you are looking for the source code of the taskybird extension for thunderbird, it's on google code - https://code.google.com/p/taskybird/source/checkout
-
-Credit goes to Axel Grude for this extension.
+a [Sails](http://sailsjs.org) application
--- api/models/User.js
+++ api/models/User.js
@@ -40,6 +40,21 @@ module.exports = {
       return obj;
     }
 
-  }
+  },
 
+  beforeCreate: function (values, next) {
+
+    // This check to make sure the password and password confirmation match before creating record
+    if (!values.password || values.password != values.confirmation) {
+      return next({err: ["Password doesn't match password confirmation."]});
+    }
+
+    require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword) {
+      if (err) return next(err);
+      values.encryptedPassword = encryptedPassword;
+      // values.online = true;
+      next();
+    });
+  }
+  
 };
